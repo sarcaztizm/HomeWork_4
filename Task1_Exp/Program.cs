a9dfc70f59b373c70a54a8b4ad4bf3f36c8602fe
Eric <ericismag@outlook.com>
2022-09-14 15:38:40 +0300

    First Program

---
+++ Task1_Exp/Program.cs
@@ -0,0 +1,10 @@
+﻿// Принять на вход два числа (А и В)
+// и возвести числа А в натуральную степень В
+
+Console.Clear();
+Console.WriteLine("Введите числа A и B");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+double Result = Math.Pow(A, B);
+Console.WriteLine($"{A} в степени {B} равно {Result}");
