430724144185ebc4df22fa9ba9fdd1baa8384bbc
Eric <ericismag@outlook.com>
2022-09-14 16:14:20 +0300

    Third Program

---
+++ Task3_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать массив из N элементов и вывести его на экран
+
+Console.Clear();
+Console.WriteLine ("Введите число элементов массива");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] Array = new int [N];
+Console.WriteLine("Массив из N элементов:");
+for (int i = 0; i < N; i++)
+{
+    Array[i] = new Random().Next(1, 100);
+    Console.Write($"{Array[i]}    ");
+}
+
