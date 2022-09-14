a722ae5c596c450deb83437dcab33b3b7589eebb
Eric <ericismag@outlook.com>
2022-09-14 16:01:43 +0300

    Fix Fisrt Program

--- Task1_Exp/Program.cs
+++ Task1_Exp/Program.cs
@@ -5,6 +5,14 @@ Console.Clear();
 Console.WriteLine("Введите числа A и B");
 int A = Convert.ToInt32(Console.ReadLine());
 int B = Convert.ToInt32(Console.ReadLine());
+double Result = 1;
 
-double Result = Math.Pow(A, B);
+// Решение с использованием мат. функицей Math.Pow
+// double Result = Math.Pow(A, B);
+// Console.WriteLine($"{A} в степени {B} равно {Result}");
+
+for (int i = 0; i < B; i++)
+{
+   Result *= A;
+}
 Console.WriteLine($"{A} в степени {B} равно {Result}");
