e850682f10bc742f775fa2b3a394471756efcdb2
Eric <ericismag@outlook.com>
2022-09-14 15:53:54 +0300

    Secord Program

---
+++ Task2_Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принять на вход два число и сосчитать
+// Сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int Nex = N;
+int Result = 0;
+
+while (N  > 0)
+{
+    Result += N % 10;
+    N /= 10;
+}
+Console.WriteLine($"Сумма цифр числа {Nex} равна {Result}");
